ff18d0ab8bd8b7e5258937b1daff2b2059b936e0
GavinChan <chenguangyu1987@126.com>
2017-02-09 11:05:13 +0800

    change encoding to utf-8 for chinese

--- src/com/getitstart/designpattern/template/test.java
+++ src/com/getitstart/designpattern/template/test.java
@@ -4,7 +4,7 @@ public class test {
 	public static void main(String args[]){
 		AbstractDisplay ad1 = new CharDisplay('H');
 		AbstractDisplay ad2 = new StringDisplay("Hello,world.");
-		AbstractDisplay ad3 = new StringDisplay("ÄãºÃ£¬ÊÀ½ç£¡");
+		AbstractDisplay ad3 = new StringDisplay("ä½ å¥½ï¼Œä¸–ç•Œï¼");
 		ad1.display();
 		ad2.display();
 		ad3.display();
