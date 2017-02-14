bda9bf597a9c4e5b31628caf718e11aa70fdedc3
GavinChan <chenguangyu1987@126.com>
2017-02-14 16:08:04 +0800

    add composite

---
+++ src/com/getitstart/designpattern/composite/FileTreatmentException.java
@@ -0,0 +1,10 @@
+package com.getitstart.designpattern.composite;
+
+public class FileTreatmentException extends RuntimeException {
+	public FileTreatmentException(){
+		
+	}
+	public FileTreatmentException(String msg){
+		super(msg);
+	}
+}
